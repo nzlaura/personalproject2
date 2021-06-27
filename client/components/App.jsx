5d04210028104c130282eeb5c0734e9aee5f6a24
Laura Corkin <nzlaura.williams@gmail.com>
2021-06-28 08:39:28 +1200

    starting to build cards

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
 // component imports
 import Title from './Title'
 import Nav from './Nav'
+import Ascia from './Ascia'
 
 function App () {
   return (
