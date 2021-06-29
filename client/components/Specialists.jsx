1bff5824ce0d0c16639014ecc252f50b077d7d08
Laura Corkin <nzlaura.williams@gmail.com>
2021-06-29 21:25:04 +1200

    accidentally working on main again

--- client/components/Specialists.jsx
+++ client/components/Specialists.jsx
@@ -1,1 +1,17 @@
 // use json to list allergy specialists
+import React, { useState} from 'react'
+
+import docData from '../data/docinfo.json'
+
+function Specialists (props) {
+
+return (
+<h1>Allergy and Immunology Specialists across NZ</h1>
+
+<ul>
+    <li key={}></li>
+</ul>
+)
+}
+
+export default Specialists
