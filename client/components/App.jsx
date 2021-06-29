0aff90015b05b7f982828e771d171297471a6ae6
Laura Corkin <nzlaura.williams@gmail.com>
2021-06-29 21:53:08 +1200

    configured app page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,6 +11,7 @@ import EpipenCard from './EpipenCard'
 import ChildDisabilityCard from './ChildDisabilityCard'
 import Sidebar from './Sidebar'
 import SpecialistCard from './SpecialistCard'
+import RecipeCard from './RecipesCard'
 
 // import components for routes
 import Specialists from './Specialists'
@@ -18,16 +19,37 @@ import Specialists from './Specialists'
 function App () {
   return (
     <>
-      {/* <Nav /> */}
+      <Nav />
       {/* <Title /> */}
-      {/* <Sidebar />
+      <Sidebar />
       <AsciaCard />
       <EpipenCard />
       <ChildDisabilityCard />
-      <SpecialistCard /> */}
-      <Specialists />
+      <SpecialistCard />
+      <RecipeCard />
     </>
   )
 }
 
 export default App
+
+// how I want the app to work
+
+// function App () {
+//   return (
+//     <>
+//     <Route path="/" component={Nav} />
+//     <Route path="/" component={Sidebar} />
+//     <Route exact path="/" component={AsciaCard} />
+//     <Route exact path="/" component={EpipenCard} />
+//     <Route exact path="/" component={ChildDisabilityCard} />
+//     <Route exact path="/" component={SpecialistCard} />
+//     <Route exact path="/" component={RecipesCard} />
+//     <Route exact path="/specialists" component={Specialists} />
+//     <Route exact path="/epipen" component={EpipenPage} />
+//     <Route exact path="/recipes" component={Recipes} />
+//     <Route exact path="/ascia" component={AsciaPage} />
+//     <Route exact path="/CDA" component={CDAPage} />
+//     </>
+//   )
+// }
